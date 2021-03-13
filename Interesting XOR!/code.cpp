91dbea15924757234c102746a832758a59fd145e
Hritika Sanjay Dhasal <66423487+HellFire03@users.noreply.github.com>
2021-03-13 09:21:03 +0530

    Update code.cpp

--- Interesting XOR!/code.cpp
+++ Interesting XOR!/code.cpp
@@ -4,21 +4,20 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-	int test;
-	cin>>test;
-	while(test--){
-	    unsigned long long int c,d=0,r,result,ans=0;
-	    cin>>c;
-        r = pow(2,(int)(log2(c)+1));
-        for(int i=1;i<r-1;i++){
-            for(int j=i+1;j<r;j++){
-                if((i^j)==c){
-                    result = i*j;
-                    result = max(result,result);
-                }
-            }
-        }
-        cout<<result<<"\n";
-	}
-	return 0;
+    int test;
+    cin>>test;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    while(test--){
+        unsigned long int C,A,B,multiply,base,ans=0;
+        cin>>C;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        base = pow(2,(int)(log2(C)+1));
+        A = (base-2)/2;
+        B = C^A;
+        multiply = A*B;
+        if(multiply>ans)
+            ans = multiply;
+        cout<<ans<<"\n";
+    }
+    return 0;
 }
