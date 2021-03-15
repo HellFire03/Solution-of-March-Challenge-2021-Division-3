2826b4b8d372ba1437bf7eb39c28f70e510e781e
Hritika Sanjay Dhasal <66423487+HellFire03@users.noreply.github.com>
2021-03-15 15:02:08 +0530

    Update code.cpp

--- Locked Safe/code.cpp
+++ Locked Safe/code.cpp
@@ -1,55 +1,34 @@
 #include <bits/stdc++.h>
-#define ll long long int
-#define ul long long unsigned int
+#define ll long long unsigned int
 using namespace std;
 
 int main() {
-    ll t;
+    ll t,n,ans;
     cin>>t;
     while(t--){
-        ll n,i,j;
         cin>>n;
-        ul arr[n];
-        for(ll i=0;i<n;i++)
-            cin>>arr[i];
-        vector<ul>v,v2,maxx;
-        ul nd = arr[0];
-        ul aor = arr[0];
-        v.push_back(nd);
-        v2.push_back(aor);
-        maxx.push_back(arr[0]);
-        ul maxx2=arr[0];
-        for(i=1;i<n;i++){
-            v.push_back(arr[i]);
-            v2.push_back(arr[i]);
-            maxx.push_back(arr[i]);
-            maxx2 = max(maxx2,arr[i]);
-            nd&=arr[i];
-            aor|=arr[i];
-            v.push_back(nd);
-            v2.push_back(aor);
-            maxx.push_back(maxx2);
+        ll number,counter=0;
+        vector<ll>vis;
+        for(ll i=0;i<n;i++){
+            cin>>number;
+            vis.push_back(number);
         }
-        for(i=n-1;i>0;i--){
-            nd=arr[i];
-            aor=arr[i];
-            maxx2=arr[i];
-            for(j=i-1;j>0;j--){
-                nd&=arr[j];
-                aor|=arr[j];
-                maxx2=max(arr[j],maxx2);
-                maxx.push_back(maxx2);
-                v.push_back(nd);
-                v2.push_back(aor);
+        ans=(n*(n-1))>>1;
+        for(ll i=1;i<n;i++){
+            if(vis[i]==vis[i-1]){
+                counter++;
             }
-            
+            else{
+                if(counter !=1){
+                    ans -= (counter*(counter-1))>>1;
+                }
+                counter = 1;
+            }
         }
-        int count =0;
-        for(i=0;i<v.size();i++){
-            if((v[i]^v2[i])>=maxx[i])
-                count++;
+        if(counter != 1){
+            ans-= (counter*(counter-1))>>1;
         }
-        cout<<count<<"\n";
+        cout<<ans<<endl;
     }
 	return 0;
 }
